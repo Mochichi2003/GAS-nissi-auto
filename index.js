362a95ca2926cd640675b71689d177a63f4c620c
Mochihchi <44772513+Mochichi2003@users.noreply.github.com>
2020-07-22 09:42:22 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/** @OnlyCurrentDoc */
+/**
+ * 
+ * @param {Number} value 体温
+ * @return {String} return その日に学校にいってるかどうか
+ * 
+ */
+function TAION(value){
+  var result 
+  if (value != "") {
+    result = "◯"
+  } else {
+    result = ""
+  }
+  return result
+}
+
+function NISSHI(value) {
+  
+}
