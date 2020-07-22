f9b1e03c3c9ef94801bd82f891c547ab5f08b855
Mochihchi <44772513+Mochichi2003@users.noreply.github.com>
2020-07-22 14:04:16 +0900

    自動で同期するのができましたああああああああああ

--- index.js
+++ index.js
@@ -26,12 +26,43 @@ function NISSHI(value) {
 
   console.log(sheet.getName());
   // return sheet.getName()
-  sheet.getRange(57, 1, 9, 9).copyTo(sheet.getRange(570, 1), { contentsOnly: true, formatOnly: true })
   var today = new Date();
-  var firstday = new Date(today.getFullYear(), 0, 1);
+  var firstday = new Date(2020, 3, 13);
+  // var firstday = new Date(today.getFullYear(), 0, 1);
+
   var fulldays = Math.floor((today.getTime() - firstday.getTime()) / (1000 * 86400));
-  document.write(fulldays + '日経過し');
-  document.write("第" + Math.floor((fulldays - today.getDay() + 12) / 7) + "週目になりました");
+  console.log(fulldays + '日経過し');
+  console.log("第" + Math.floor((fulldays - today.getDay() + 12) / 7) + "週目になりました");
+
+  var Week_passed = Math.floor((fulldays) / 7)
+  console.log(Week_passed);
+  var Week_line_length = 16 + ((Week_passed) * 10) - 2
+  console.log("行番号 = " + Week_line_length)
+  sheet.getRange(5, 1, 9, 7).copyTo(sheet.getRange(Week_line_length, 1), { contentsOnly: true, formatOnly: true })//コピペするとこ
+  sheet.getRange(Week_line_length, 1, 9, 7).copyTo(sheet.getRange(5, 1), { contentsOnly: true, formatOnly: true })//コピペするとこ
   // touday_nisshi.getValue()
 
+}
+
+function SYuhazime(params) {
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = ss.getSheetByName("③日誌");
+
+  console.log(sheet.getName());
+  // return sheet.getName()
+  var today = new Date();
+  var firstday = new Date(2020, 3, 13);
+  // var firstday = new Date(today.getFullYear(), 0, 1);
+
+  var fulldays = Math.floor((today.getTime() - firstday.getTime()) / (1000 * 86400));
+  console.log(fulldays + '日経過し');
+  console.log("第" + Math.floor((fulldays - today.getDay() + 12) / 7) + "週目になりました");
+
+  var Week_passed = Math.floor((fulldays) / 7)
+  console.log(Week_passed);
+  var Week_line_length = 16 + ((Week_passed) * 10) - 2
+  console.log("行番号 = " + Week_line_length)
+  sheet.getRange(Week_line_length, 1, 9, 7).copyTo(sheet.getRange(5, 1), { contentsOnly: true, formatOnly: true })//コピペするとこ
+  sheet.getRange(5, 1, 9, 7).copyTo(sheet.getRange(Week_line_length, 1), { contentsOnly: true, formatOnly: true })//コピペするとこ
+  // touday_nisshi.getValue()
 }
